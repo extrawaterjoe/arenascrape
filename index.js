ff53ebc0030a9356c75d958acab7baa75e257cbc
joe lazarz <jlazarz87@gmail.com>
2022-07-25 23:24:43 -0400

    drawing imgs to canvas

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+import fs from 'fs'
+import path from 'path'
+import https from 'https'
+import * as url from 'url'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+import { Image, createCanvas } from 'canvas'
+import fetch from 'node-fetch'
+;(async () => {
+  const canvas = createCanvas(595, 842)
+  const ctx = canvas.getContext('2d')
+  ctx.fillStyle = '#fff'
+  ctx.fillRect(0, 0, 595, 842)
 
-(async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto('* any are.na channel url *');
-	await page.evaluate(async () => {
-		await new Promise((resolve, reject) => {
-			let totalHeight = 0;
-			let distance = 100;
-			let timer = setInterval(() => {
-				let scrollHeight = document.body.scrollHeight;
-				window.scrollBy(0, distance);
-				totalHeight += distance;
+  const res = await fetch('https://api.are.na/v2/channels/extra-water?page=1&amp;per=50')
+  const data = await res.json()
 
-				if (totalHeight >= scrollHeight) {
-					clearInterval(timer);
-					resolve();
-				}
-			}, 500);
-		});
-	});
-	await page.waitForSelector('img');
-	const imageUrls = await page.evaluate(() => {
-		const imageUrls = Array.from(document.querySelectorAll('img')).map(
-			(img) => {
-				const srcset = img.srcset;
-				const src = srcset.match(/2x,\s(.*)\s/)[1];
-				return src;
-			}
-		);
-		return imageUrls;
-	});
-	imageUrls.forEach((url, index) => {
-		https.get(url, (res) => {
-			res.pipe(
-				fs.createWriteStream(path.join(__dirname, `images/${index}.jpg`))
-			);
-		});
-	});
+  const images = data.contents
+    .filter((item) => item.class === 'Image')
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 4)
 
-	await browser.close();
-})();
+  images.forEach((image, index) => {
+    const fileName = String(index) + '.png'
+    const filePath = path.join(__dirname, 'source', fileName)
+    const file = fs.createWriteStream(filePath)
+    const request = https.get(image.image.large.url, (response) => {
+      response.pipe(file)
+    })
+  })
+
+  setTimeout(() => {
+    const files = fs.readdirSync(path.join(__dirname, 'source'))
+    files.forEach((file, index) => {
+      const img = new Image()
+      img.src = path.join(__dirname, 'source', file)
+
+      // draw images on to ctx canvas in a 2x2 grid with margin
+      ctx.drawImage(img, (index % 2) * 300, Math.floor(index / 2) * 300)
+
+      canvas.toBuffer((err, buffer) => {
+        fs.writeFileSync(path.join(__dirname, 'output', 'output.png'), buffer)
+      })
+    })
+  }, 9000)
+})()
